79d69f29a3f5eeef73167a5d83273cc487d355b5
muhammad hunfa jalil <hunfa.jalil786@gmail.com>
2023-05-25 12:46:44 +0500

    dasd

--- pages/dashboard/addorder.js
+++ pages/dashboard/addorder.js
@@ -228,54 +228,9 @@ function AddOrder() {
           {snackbar.msg}
         </Alert>
       </Snackbar>
-      <TableContainer
-        component={Paper}
-        sx={{ width: "97%", mx: "auto", marginTop: "20px" }}
-      >
-        <Table sx={{ minWidth: 700 }} aria-label="spanning table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" colSpan={3}>
-                Details
-              </TableCell>
-              <TableCell align="right">Price</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Desc</TableCell>
-              <TableCell align="right">Qty.</TableCell>
-              <TableCell align="right">Unit</TableCell>
-              <TableCell align="right">Sum</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.code}>
-                <TableCell>{row.desc}</TableCell>
-                <TableCell align="right">{row.qty}</TableCell>
-                <TableCell align="right">{row.unit}</TableCell>
-                <TableCell align="right">{ccyFormat(row.price)}</TableCell>
-              </TableRow>
-            ))}
-
-            <TableRow>
-              <TableCell rowSpan={3} />
-              <TableCell colSpan={2}>Subtotal</TableCell>
-              <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Tax</TableCell>
-              <TableCell align="right">{`${(TAX_RATE * 100).toFixed(
-                0
-              )} %`}</TableCell>
-              <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell colSpan={2}>Total</TableCell>
-              <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <AddOrderTable rows={rows} invoiceSubtotal={screenData.invoiceSubtotal}  invoiceDiscount={screenData.invoiceDiscount} invoiceTotal={screenData.invoiceTotal}
+     handledeleteRow={handledeleteRow}
+     />
 
       <Paper
         sx={{
